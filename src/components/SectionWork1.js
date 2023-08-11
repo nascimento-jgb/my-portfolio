c251fd87c9cbad7bb96874f684cd54c072195e6f
Joao Nascimento <nascimento.joaogb@gmail.com>
2023-08-11 14:05:03 +0300

    pics changed

--- src/components/SectionWork1.js
+++ src/components/SectionWork1.js
@@ -34,9 +34,9 @@ const SectionWork1 = () => {
 			</a>
 			<div className="absolute right-0 top-1/2 transform -translate-y-1/2 -translate-x-1/4">
 				<img
-					src="flipped-profile.png"
+					src="mockup-group.png"
 					alt="Profile"
-					className="h-[700px] opacity-90"
+					className="h-[700px] w-[600px] opacity-90"
 				/>
 			</div>
 		</div>
